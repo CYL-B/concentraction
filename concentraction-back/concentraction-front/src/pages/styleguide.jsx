e91fd45ceda1d7ef338eba68c2c678e18f3e012b
Camille Benhammadi <benhammadcamille@hotmail.fr>
2024-03-12 00:40:00 +0100

    added margins to app, updated heading2, corrected header element, fixed layout component

--- concentraction-back/concentraction-front/src/pages/styleguide.jsx
+++ concentraction-back/concentraction-front/src/pages/styleguide.jsx
@@ -1,22 +1,30 @@
-import { Heading1, Heading2, Heading3, Heading4, Body, Fineprint} from "../components/typography";
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Body,
+  Fineprint,
+} from "../components/typography";
+
+ import Layout from "../components/layout";
 
 export default function Styleguide() {
-
-    return (
-      <>
-         <div id="Styleguide">
-            <section className="typography">
-                <Heading1>Heading 1</Heading1>
-                <Heading2>Heading 2</Heading2>
-                <Heading3>Heading 3</Heading3>
-                <Heading4>Heading 4</Heading4>
-                <Body>Body</Body>
-                <Body body2={true}>Body 2</Body>
-                <Fineprint>Fineprint</Fineprint>
-            </section>
-            
-            </div>
-      </>
-    )
-  }
-    
+  return (
+    <>
+      <div id="Styleguide">
+        <Layout headerTitle="Styleguide">
+          <section className="typography">
+            <Heading1>Heading 1</Heading1>
+            <Heading2>Heading 2</Heading2>
+            <Heading3>Heading 3</Heading3>
+            <Heading4>Heading 4</Heading4>
+            <Body>Body</Body>
+            <Body body2={true}>Body 2</Body>
+            <Fineprint>Fineprint</Fineprint>
+          </section>
+          </Layout>
+      </div>
+    </>
+  );
+}
