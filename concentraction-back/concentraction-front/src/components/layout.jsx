a89512b8d970f933c61ce7f28d44b24cd9d5b66e
Camille Benhammadi <benhammadcamille@hotmail.fr>
2024-03-14 00:35:46 +0100

    styling buttons, icon+button, links, changed font nanoto, changed layout so menu is in absolute position relatively to the whole page, defined outline at app level

--- concentraction-back/concentraction-front/src/components/layout.jsx
+++ concentraction-back/concentraction-front/src/components/layout.jsx
@@ -4,17 +4,24 @@ function Container({ children, className }) {
   return <div className={`container ${className}`}>{children}</div>;
 }
 
-export default function Layout({ children, containerClassName, headerTitle, mainClassName }) {
+export default function Layout({
+  children,
+  containerClassName,
+  headerTitle,
+  mainClassName,
+  id,
+  pageClassName
+}) {
   return (
     <>
-    <Header headerTitle={headerTitle}></Header>
-      <div className={`container mx-auto ${containerClassName ?? ""}`}>
-        <aside className="md:w-1/6">
-
-        </aside>
-        <main role="main" className={`w-full ${mainClassName ?? ""}`}>
-        {children}
-        </main>
+      <div id={id} className={`relative p-10 ${pageClassName ?? ""}`}>
+        <Header headerTitle={headerTitle}></Header>
+        <div className={`container mx-auto ${containerClassName ?? ""}`}>
+          <aside className="absolute h-screen top-0 left-0 md:w-1/6"></aside>
+          <main role="main" className={`w-full ${mainClassName ?? ""}`}>
+            {children}
+          </main>
+        </div>
       </div>
     </>
   );
--- concentraction-back/concentraction-front/src/components/links.jsx
+++ concentraction-back/concentraction-front/src/components/links.jsx
@@ -1,5 +1,18 @@
-export function Link() {
-    return(
-        <a></a>
-    )
+const variantMaps = {
+  primary: "",
+  secondary: "",
+};
+
+export function Link({ onClick, variant, children }) {
+  const linkLayoutClasses = "";
+
+  const finalLinkClasses = `${linkLayoutClasses} ${
+    variantMaps[variant.toLowerCase()]
+  }`;
+
+  return (
+    <a href="#" role="button" onClick={onClick} className={finalLinkClasses}>
+      {children}
+    </a>
+  );
 }
