f743c908ea1ef245640b618cd55601d7e649d6b3
Camille Benhammadi <benhammadcamille@hotmail.fr>
2024-04-06 00:51:06 +0200

    styled dropdown

--- concentraction-back/concentraction-front/src/components/input/dropdown.jsx
+++ concentraction-back/concentraction-front/src/components/input/dropdown.jsx
@@ -1,13 +1,32 @@
-export function Dropdown({ children, ...restDropProps }) {
+import IconifyIcon from "../icon";
+export function Dropdown({
+  register,
+  options,
+  name,
+  variant = "text-light",
+  ...rest
+}) {
   // const [option, setOption] = useState("");
 
   // const handleDropdown = (event) => {
   //   setOption(event.target.value)
   // }
 
-  return <select {...restDropProps}>{children}</select>;
-}
-
-export function Option({ value, name }) {
-  return <option value={value}>{name}</option>;
+  return (
+    <div className={`dropdown relative  ${variant.toLowerCase()}`}>
+      <select
+        {...register(name)}
+        {...rest}
+        className={`pb-4 appearance-none bg-transparent w-full font-bold`}
+      >
+        <option value="" disabled selected>Dropdown</option>
+        {options.map((value) => (
+          <option key={value} value={value}>
+            {value}
+          </option>
+        ))}
+      </select>
+      <IconifyIcon iconName="raphael:arrowdown" width={20} height={20} iconClassName="absolute right-4"/>
+    </div>
+  );
 }
