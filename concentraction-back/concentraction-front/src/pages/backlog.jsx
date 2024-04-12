4e49950a40825f4a0a586ac2a612da4437143164
Camille Benhammadi <benhammadcamille@hotmail.fr>
2024-04-13 00:20:19 +0200

    backlog layout and tabs

--- concentraction-back/concentraction-front/src/pages/backlog.jsx
+++ concentraction-back/concentraction-front/src/pages/backlog.jsx
@@ -1,11 +1,28 @@
+import Layout from "../components/layout/layout";
+import Tabs from "../components/tabs";
+import { DayView, WeekView, MonthView } from "../components/layout/views";
 
-export default function Backlog() {
+export default function Dashboard() {
 
-    return (
-      <>
-         <div id="Backlog">I am the backlog page</div>
-      </>
-    )
-  }
+  const tabButtons = ["Month view", "Day view", "Week view"];
   
-  
+  return (
+    <Layout id="Backlog" headerTitle="Day">
+      <Tabs
+        tabsIds={tabButtons}
+        getHeader={(tabId) => {
+          return tabId;
+        }}
+        renderContent={(tabId) => {
+          if (tabId === "Day view") {
+            return (<DayView/>)
+          } else if (tabId === "Week view") {
+            return (<WeekView/>)
+          } else {
+            return (<MonthView/>)
+          }
+        }}
+      />
+    </Layout>
+  );
+}
