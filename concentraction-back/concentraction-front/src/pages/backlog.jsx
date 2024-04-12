d740e517ffb78578bdb454a92ebae9c499007347
Camille Benhammadi <benhammadcamille@hotmail.fr>
2024-04-13 00:21:04 +0200

    removed backlog layout, confused with dashboard layout

--- concentraction-back/concentraction-front/src/pages/backlog.jsx
+++ concentraction-back/concentraction-front/src/pages/backlog.jsx
@@ -1,28 +1,11 @@
-import Layout from "../components/layout/layout";
-import Tabs from "../components/tabs";
-import { DayView, WeekView, MonthView } from "../components/layout/views";
 
-export default function Dashboard() {
+export default function Backlog() {
 
-  const tabButtons = ["Month view", "Day view", "Week view"];
+    return (
+      <>
+         <div id="Backlog">I am the backlog page</div>
+      </>
+    )
+  }
   
-  return (
-    <Layout id="Backlog" headerTitle="Day">
-      <Tabs
-        tabsIds={tabButtons}
-        getHeader={(tabId) => {
-          return tabId;
-        }}
-        renderContent={(tabId) => {
-          if (tabId === "Day view") {
-            return (<DayView/>)
-          } else if (tabId === "Week view") {
-            return (<WeekView/>)
-          } else {
-            return (<MonthView/>)
-          }
-        }}
-      />
-    </Layout>
-  );
-}
+  
